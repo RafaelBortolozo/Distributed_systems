7d78a07713a59c1e94d71ffbb90f2105980ca544
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-04-24 04:28:06 -0300

    update

--- sockets/questao3/readme.md
+++ sockets/questao3/readme.md
@@ -1,1 +1,20 @@
-a
+# Perguntas e respostas via socket
+
+- Python version 3.9.7
+
+Aplicação que apresenta perguntas de multipla escolha ao cliente. Ao final é apresentado o total de questões e quantas acertou/errou.
+
+Para executar basta rodar os comandos abaixo:
+
+Servidor:
+
+```
+    python server.py
+```
+
+Cliente:
+
+```
+    python client.py
+```
+
---
+++ sockets/questao3/server.py
@@ -0,0 +1,63 @@
+import socket
+import os
+
+IP = '127.0.0.1'
+PORT = int(input('Server Port: '))
+ARQUIVES_ADDRESS = str(input('Server Arquives Path: '))
+
+# PORT = 7777
+# ARQUIVES_ADDRESS = 'arquives_server/'
+
+def progressPercent(currentSize, totalSize):
+    try:
+        return int(currentSize*100/totalSize)
+    except ZeroDivisionError:
+        return 0
+
+# Cria objeto socket especificando o protocolo IPV4 e o tipo de comunicacao (TCP neste caso)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Liga o objeto socket ao endereco do servidor
+server.bind((IP, PORT))
+
+# Com o endereco e porta definidos, agora o servidor pode ouvir outras conexoes socket 
+server.listen()
+print(f"\nListening on {IP}:{PORT}")
+
+# Todas as conexoes serao aceitas com o seguinte comando
+# cliente_socket: Objeto socket do cliente
+# address: Endereco IP do cliente
+client_socket, address = server.accept()
+print(f"{address} is connected.\n")
+
+
+#******************** RECEBER NOME DO ARQUIVO E ENVIAR O ARQUIVO AO CLIENTE ********************#
+
+# Recebe o nome do arquivo que o cliente quer, ja convertido em string (originalmente retorna varios bytes)
+nameFileRequested = client_socket.recv(1024).decode()
+
+# Concatena com o endereco da pasta de arquivos
+addressFileRequested = ARQUIVES_ADDRESS + nameFileRequested
+
+# Pega tamanho do arquivo (uso em print)
+fileSize = os.path.getsize(addressFileRequested)
+
+# Envia ao cliente o tamanho do arquivo (uso em print)
+client_socket.send(("fileSize:{}".format(fileSize)).encode('utf-8'))
+
+# Abre o arquivo lendo somente os bytes (rb) e envia o tamanho do arquivo
+count = 0
+with open(addressFileRequested, 'rb') as file:
+    # Envie os bytes do arquivo em partes (atraves do for() percorrendo os bytes do arquivo)
+    print("Uploading: {}...".format(nameFileRequested))
+    for data in file.readlines():
+        client_socket.send(data)
+
+        ## Opcao de imprimir porcentagem, mas o servidor ##
+        ## fica lento e certamente vai corromper o arquivo ##
+
+        #count += len(data)
+        #print("Uploading: {} ({}%)...".format(nameFileRequested, progressPercent(count, fileSize)), end='\r')
+        
+    print("\nUploaded!")
+
