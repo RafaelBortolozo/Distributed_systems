ede93003c624b94263180ddce262824de26dcb4e
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-04-27 10:40:44 -0300

    client-side working

--- sockets/listsManager/Client.java
+++ sockets/listsManager/Client.java
@@ -1,4 +1,4 @@
-package questao1;
+package listsManager;
 
 import java.io.*;
 import java.net.Socket;
@@ -6,24 +6,42 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        int servidorPorta = 1234;
-        String servidorIP = "127.0.0.1";
-        Socket socket = new Socket(servidorIP, servidorPorta);
-        Scanner in = new Scanner(System.in);
+        String ip = "127.0.0.1";
+        int port = 7777;
+        Socket socket = new Socket(ip, port);
+        Scanner scanner = new Scanner(System.in);
 
         DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
+        String menu = null;
+        String serverMessage = null;
+        int opt = -1;
+
         while(true){
-            String message = inputStream.readUTF();
-            System.out.println(message);
-
-            if(message.equals("Sair")){
-                break;
-            }
-
-            outputStream.writeUTF(in.nextLine());
+            menu = inputStream.readUTF();
+            System.out.println(menu);
+            opt = Integer.parseInt(scanner.nextLine());
+            outputStream.writeUTF(Integer.toString(opt));
             outputStream.flush();
+
+            if(opt > 0 && opt < 8){
+                if (opt == 1 || opt == 3){
+                    System.out.println(inputStream.readUTF());
+                    outputStream.writeUTF(scanner.nextLine());
+                    System.out.println(inputStream.readUTF());
+                } else if (opt == 2 || opt == 4){
+                    System.out.println(inputStream.readUTF());
+                    outputStream.writeUTF(scanner.nextLine());
+                    System.out.println(inputStream.readUTF());
+                    outputStream.writeUTF(scanner.nextLine());
+                    System.out.println(inputStream.readUTF());
+                }else{
+                    serverMessage = inputStream.readUTF();
+                    System.out.println(serverMessage);
+                    if(serverMessage.equals("Conexão encerrada!")) break;
+                }
+            }
         }
     }
 }
