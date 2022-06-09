caa558c7aa0d836538c04641ec5898ff89619676
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-06-09 10:56:06 -0300

    updated

--- agenda/src/test/java/ifc/springBoot/agenda/exception/PessoaNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package ifc.springBoot.agenda.exception;
-
-public class PessoaNaoEncontradaException extends RuntimeException {
-    public PessoaNaoEncontradaException(Integer id){
-        super("Não possível encontrar o contato com o id: " + id);
-    }
-}
