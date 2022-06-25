a97c24439cc12c644a39c94c5ce75e9d559d794a
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2022-06-25 18:30:01 -0300

    initial commit

--- ring_algorithm/client.py
+++ ring_algorithm/client.py
@@ -1,40 +1,38 @@
-from timeit import default_timer as timer
-from dateutil import parser
-import threading
-import datetime
-import socket
+import Pyro4
+import os
 import time
 
+def clientLogin():
+    print("\nLOGIN")
+    name = str(input("Digite o seu nome de usuario: "))
+    password = str(input("Digite a senha: "))
+    print("Verificando credenciais...")
+    authenticated = quiz.clientLogin(name, password)
+    if authenticated:
+        print("Autenticado.")
+        return
+    else: 
+        print("login invalido, tente novamente.")
+        clientLogin()
 
-def sendTime(slave_client):
-    while True:
-        slave_client.send(str(datetime.datetime.now()).encode())
-        print("Time sent!\n\n")
-        time.sleep(5)
+def answerQuiz():
+    maxQuestions = int(input("\nDigite o máximo de questões que deseja responder: "))
+    quiz.generateRandomQuestions(maxQuestions)
+    totalQuestions = quiz.getLengthRandomQuestions()
+    for index in range(totalQuestions):
+        question = quiz.nextQuestion()
+        os.system('clear') 
+        print("\n{} - {}".format((index+1),question))
+        clientAnswer = int(input("Opcao: "))
+        feedback = quiz.answerIsCorrect(question, clientAnswer) 
+        time.sleep(1)
+        print(feedback)
+        time.sleep(1)
+    
+    result = quiz.getResultQuiz()
+    os.system('clear')
+    print(result)
 
-
-def receiveTime(slave_client):
-    while True:
-        sync_time = parser.parse(slave_client.recv(1024).decode())
-
-        # String to time
-        str_time = datetime.datetime.strftime(sync_time, '%H:%M:%S')
-
-        print("Sync time:", str_time)
-
-
-def initSlaveClient(port=8000):
-    slave_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    slave_client.connect(('localhost', port))
-
-    slave_thread = threading.Thread(target=receiveTime, args=(slave_client,))
-    slave_thread.start()
-
-    print("Receiveng time from server...")
-    receive_time_thread = threading.Thread(
-        target=sendTime, args=(slave_client,))
-    receive_time_thread.start()
-
-
-if __name__ == '__main__':
-    initSlaveClient()
+quiz = Pyro4.Proxy("PYRONAME:quiz")
+clientLogin()
+answerQuiz()
--- ring_algorithm/server.py
+++ ring_algorithm/server.py
@@ -1,87 +1,98 @@
-from functools import reduce
-from dateutil import parser
-import threading
-import datetime
-import socket
-import time
+import Pyro4
+import io
+import random
 
-# Object to store client IP and Clock data
-client_data = {}
+from sqlalchemy import null, true
 
+@Pyro4.expose
+class Quiz(object):
+    def __init__(self):
+        self.randomQuestions = 0
+        self.clientCorrectAnswers = 0
+        self.count = 0
 
-def receiveClockTime(connector, address):
-    # Receive the clock time from the client
-    while True:
-        clock_time_string = connector.recv(1024).decode()
-        clock_time = parser.parse(clock_time_string)
-        clock_time_difference = datetime.datetime.now() - clock_time
+    def getAllQuestions(self, arquive):
+        questions = []
+        question = ""
+        answer = 0
+        count = 0
+        with io.open(arquive, "r", encoding="utf8") as arq:
+            for line in arq.readlines():
+                #salva o titulo e as opcoes
+                if count >= 0 and count < 5:
+                    question += str(line)
+                    count += 1
 
-        client_data[address] = {
-            'clock_time': clock_time,
-            'clock_time_difference': clock_time_difference,
-            "connector": connector
-        }
+                #salva a opcao correta e reinicia contador
+                else:
+                    answer = int(line)
+                    questions.append({
+                        "question": question.upper(),
+                        "answer": answer
+                    })
+                    question = ""
+                    count = 0
+        return questions
 
-        print("Client data updated:", str(address), '\n\n')
-        time.sleep(5)
+    def generateRandomQuestions(self, maxQuestions):
+        allQuestions = self.getAllQuestions("questions.txt")
+        random.shuffle(allQuestions)
+        self.randomQuestions = allQuestions[:maxQuestions]
 
+    def getLengthRandomQuestions(self):
+        return len(self.randomQuestions)
 
-def makeConnection(master_server):
-    while True:
-        master_slave_connector, address = master_server.accept()
-        slave_address = str(address[0] + ":" + str(address[1]))
-
-        print(slave_address, "connected")
-
-        receiveClockTime(master_slave_connector, slave_address)
-
-
-def getAverage():
-    current_client = client_data.copy()
-
-    time_difference_list = []
-    for client_addr, client in current_client.items():
-        time_difference_list.append(client['clock_time_difference'])
-
-    clock_differences_sum = sum(time_difference_list, datetime.timedelta(0, 0))
-
-    average_clock_difference = clock_differences_sum / \
-        len(time_difference_list)
-
-    return average_clock_difference
+    def nextQuestion(self):
+        nextQuestion = self.randomQuestions[self.count]
+        self.count += 1
+        return nextQuestion["question"]
 
 
-def syncClocks():
-    while True:
-        print("Num of clients", len(client_data))
+    def getNamesAndPasswords(self, arquive):
+        students = []
+        name = ''
+        password = ''
+        with io.open(arquive, "r", encoding="utf8") as arq:
+            for line in arq.readlines():
+                aux = line.strip('\n').split(":")
+                name = aux[0]
+                password = aux[1]
+                students.append({
+                    'name': name,
+                    'password': password
+                })
+        return students
 
-        if len(client_data) > 0:
-            average_clock_difference = getAverage()
+    def getResultQuiz(self):
+        print("client finished quiz.")
+        return f"Questionário finalizado :)\nVocê acertou {self.clientCorrectAnswers}/{len(self.randomQuestions)} questões."
 
-            for client_addr, client in client_data.items():
-                try:
-                    time_sync = datetime.datetime.now() + average_clock_difference
-                    client['connector'].send(str(time_sync).encode())
-                except:
-                    print("Error sending time to client")
-        else:
-            print("No clients connected")
+    def clientLogin(self, name, password):
+        def verifyLogin(name, password):
+            for student in self.getNamesAndPasswords("authorizedUsers.txt"):
+                if (student['name'] == name and student['password'] == password):
+                    print("client logged")
+                    return True
+            
+            return False
 
-        time.sleep(5)
+        print("client logging...")
+        authenticated = verifyLogin(name, password)
+        return authenticated
 
+    def answerIsCorrect(self, question, clientAnswer):
+        for obj in self.randomQuestions:
+            if (obj["question"]) == question and obj["answer"] == clientAnswer:
+                self.clientCorrectAnswers += 1
+                return "Certa resposta!"
 
-def initMasterDaemon(port=8000):
-    master_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    master_server.bind(('', port))
-    master_server.listen(5)
+        return "Que pena, você errou."
 
-    current_thread = threading.Thread(
-        target=makeConnection, args=(master_server,))
-    current_thread.start()
+    
+daemon = Pyro4.Daemon()    # make a Pyro daemon
+ns = Pyro4.locateNS()      # find the name server
+uri = daemon.register(Quiz)# register the greeting maker as a Pyro object
+ns.register("quiz", uri)   # register the object with a name in the name server
 
-    sync_thread = threading.Thread(target=syncClocks, args=())
-    sync_thread.start()
-
-
-if __name__ == '__main__':
-    initMasterDaemon()
+print("client conected.")
+daemon.requestLoop()       # start the event loop of the server to wait for calls
